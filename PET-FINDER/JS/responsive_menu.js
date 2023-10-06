8b8fd23bad8c87a2a086aec403e75632d3be96f0
Mauro Exequiel Pepa <mauropepa1997oficial@gmail.com>
2023-10-06 07:51:42 -0300

    Deployd-files

--- PET-FINDER/JS/responsive_menu.js
+++
@@ -1,9 +0,0 @@
-var navMenu = document.querySelector(".nav-pointer");
-var navbarLinks = document.querySelector(".links-navbar");
-
-function loadMenu() {
-    navbarLinks.classList.toggle("mobile-menu");
-    navMenu.classList.toggle("open");
-}
-
-navMenu.addEventListener("click", loadMenu);
